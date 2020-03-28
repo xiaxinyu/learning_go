ec47c39ec2bbd41c8929251d0883f66730129539
summer.xia <summer_west2010@126.com>
2020-03-28 23:14:23 +0800

    git test

--- base/HelloWorld.go
+++ base/HelloWorld.go
@@ -5,7 +5,10 @@ import "fmt"
 func main() {
 	const pi = 3.1415926
 	println("Hello World")
-	fmt.Println("Hello World2")
+
 	fmt.Println(pi)
 	fmt.Println("pi=", pi)
+
+	name := "summer"
+	fmt.Println("name=", name)
 }
