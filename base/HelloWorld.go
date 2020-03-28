b9f19d06813bf276bb2bd0dcb1ba6c1688463109
summer.xia <summer_west2010@126.com>
2020-03-28 23:03:01 +0800

    git test

--- base/HelloWorld.go
+++ base/HelloWorld.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	const pi  = 3.1415926
+	const pi = 3.1415926
 	println("Hello World")
 	fmt.Println("Hello World2")
 	fmt.Println(pi)
+	fmt.Println("pi=", pi)
 }
