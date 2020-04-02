b5aa3ce9fd3cd6530415d19c4ae043f227f7b76c
summer.xia <summer_west2010@126.com>
2020-04-02 17:52:42 +0800

    add strconv error

--- string/strconv1.go
+++ string/strconv1.go
@@ -8,4 +8,8 @@ import (
 func main() {
 	s,_ := strconv.ParseInt("100", 10, 64)
 	fmt.Printf("t=%T, s=%d",s, s)
+
+	s1, err := strconv.ParseInt("ui", 10, 64)
+	fmt.Println(err)
+	fmt.Printf("t=%T, s=%d",s1, s1)
 }
